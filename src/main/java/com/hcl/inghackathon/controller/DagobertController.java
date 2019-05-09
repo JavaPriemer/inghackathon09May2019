a739aea4ab105f5998d6280001ea582195bd5455
user1 <user1@LP-71VDTS1.HCLT.CORP.HCL.IN>
2019-05-09 14:40:39 +0530

    payment approval

--- src/main/java/com/hcl/inghackathon/controller/DagobertController.java
+++ src/main/java/com/hcl/inghackathon/controller/DagobertController.java
@@ -2,14 +2,20 @@ package com.hcl.inghackathon.controller;
 
 import java.util.List;
 
+import org.omg.IOP.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.inghackathon.entities.Source;
+import com.hcl.inghackathon.entities.Transaction;
+import com.hcl.inghackathon.model.TransactionModel;
 import com.hcl.inghackathon.service.CommissionService;
+import com.hcl.inghackathon.service.PaymentApprovalServcice;
 import com.hcl.inghackathon.service.SourceService;
 
 @RestController
@@ -21,6 +27,10 @@ public class DagobertController {
 
 	@Autowired
 	CommissionService serviceFour;
+	
+	@Autowired
+	PaymentApprovalServcice paymentservice;
+	
 
 	@GetMapping("/viewAllServiceTransactions")
 	public List<Source> getAllServiceTransactions(@RequestParam("partyId") Long partyId,
@@ -35,5 +45,19 @@ public class DagobertController {
 		Double calculatedCommission = serviceFour.getCalculatedCommission(partyId, activityCode, productCode);
 		return calculatedCommission;
 	}
+	
+	@GetMapping("/getPendingPayments")
+	public List<Transaction> getPaymentToApprove(@RequestParam("partyId") Long partyId) {
+		List<Transaction> approvedList = paymentservice.getPendingPayments(partyId);
+		return approvedList;
+	}
+	
+	@PostMapping("/approvePayment")
+	public boolean approvePayment(@RequestBody Transaction transaction) {
+		boolean status = paymentservice.approvePayments(transaction);
+		return status;
+	}
+	
+	
 
 }
--- src/main/java/com/hcl/inghackathon/repository/TransactionRepository.java
+++ src/main/java/com/hcl/inghackathon/repository/TransactionRepository.java
@@ -1,11 +1,19 @@
 package com.hcl.inghackathon.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hcl.inghackathon.entities.Transaction;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+	@Query(value = "select transaction_id, party_id, amount_to_be_paid, approval_status  from transaction "
+			+ "where approval_status not in ('approved')" + "and party_id = :partyId", nativeQuery = true)
+	public List<Transaction> getOfUnapprovedTransactions(@Param("partyId") Long party_id);
 
 }
